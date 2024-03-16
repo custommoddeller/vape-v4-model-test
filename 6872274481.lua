258cb47f7c4e9fe94fd45f9c8ccaa4bdb7057e63
custommoddeller <163665395+custommoddeller@users.noreply.github.com>
2024-03-16 17:05:23 +0000

    Heatseeker attempt

--- 6872274481.lua
+++ 6872274481.lua
@@ -4844,10 +4844,21 @@ runFunction(function()
 		Name = "Speed",
 		Function = function(callback)
 			if callback then
+                task.spawn(function()
+                    while wait(4)
+                        if not callback then break end
+                        ospeed = SpeedValue.Value
+                        SpeedValue.Value = ospeed + 10
+                        task.wait(0.12)
+                        SpeedValue.Value = ospeed
+                        ospeed = SpeedValue.Value
+                    end
+                end)
+
 				table.insert(Speed.Connections, vapeEvents.EntityDamageEvent.Event:Connect(function(damageTable)
 					if damageTable.entityInstance == lplr.Character and (damageTable.damageType ~= 0 or damageTable.extra and damageTable.extra.chargeRatio ~= nil) and (not (damageTable.knockbackMultiplier and damageTable.knockbackMultiplier.disabled or damageTable.knockbackMultiplier and damageTable.knockbackMultiplier.horizontal == 0)) and SpeedDamageBoost.Enabled then 
 						damagetick = tick() + 0.4
-					end
+                    end
 				end))
 				RunLoops:BindToHeartbeat("Speed", function(delta)
 					if GuiLibrary.ObjectsThatCanBeSaved["Lobby CheckToggle"].Api.Enabled then

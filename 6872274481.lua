8a93f3d9640f2191a2fa11e6ed92c4facb4b4c13
custommoddeller <163665395+custommoddeller@users.noreply.github.com>
2024-03-16 21:59:12 +0000

    Fly.Enabled

--- 6872274481.lua
+++ 6872274481.lua
@@ -2816,23 +2816,15 @@ runFunction(function()
 		Function = function(callback)
 			if callback then
                 task.spawn(function()
-                    if not fly.Enabled then return end
                     oflyspeed = FlySpeed.Value
-                    if not fly.Enabled then return end
                     FlySpeed.Value = 55
-                    if not fly.Enabled then return end
                     task.wait(0.11)
-                    if not fly.Enabled then return end
                     FlySpeed.Value = oflyspeed
-                    if not fly.Enabled then return end
                     oflyspeed = FlySpeed.Value
-                    if not fly.Enabled then return end
                     task.wait(2)
-                    if not fly.Enabled then return end
+                    if not Fly.Enabled then return end
                     FlySpeed.Value = 55
-                    if not fly.Enabled then return end
                     task.wait(0.11)
-                    if not fly.Enabled then return end
                     FlySpeed.Value = oflyspeed
                 end)
 

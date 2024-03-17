dbb61ad1f1ef89c6ab1a127f5d2427afb67c1ca8
custommoddeller <163665395+custommoddeller@users.noreply.github.com>
2024-03-17 15:14:02 +0000

    Update 6872274481.lua

--- 6872274481.lua
+++ 6872274481.lua
@@ -2794,9 +2794,9 @@ runFunction(function()
 						workspace.Gravity = 0
 						entityLibrary.character.HumanoidRootPart.Velocity = Vector3.new(0, ((GravityFlyUp and GravityFlyVerticalSpeed.Value) or (GravityFlyDown and -GravityFlyVerticalSpeed.Value) or 0), 0)
 						local flyray = getPlacedBlock(entityLibrary.character.HumanoidRootPart.Position + Vector3.new(0, (entityLibrary.character.Humanoid.HipHeight * -2) - 1, 0))
-						if (flygroundtime - tick()) < 0.6 and not onground then
+						if (flygroundtime - tick()) < 0.9 and not onground then
 							if GravityFlyHighjump.Enabled then
-								warningNotification("GravityFly", "Highjumped! Magnitude: " .. tostring((entityLibrary.character.HumanoidRootPart.Velocity).magnitude), 2.5)
+								warningNotification("GravityFly", "Highjumped", 2.5)
 								entityLibrary.character.HumanoidRootPart.Velocity = Vector3.new(entityLibrary.character.HumanoidRootPart.Velocity.X, 310, entityLibrary.character.HumanoidRootPart.Velocity.Z)
 							end
 						end

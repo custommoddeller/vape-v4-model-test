2fcc251eed88a79be758f6fb4c154770bf49f69d
custommoddeller <163665395+custommoddeller@users.noreply.github.com>
2024-03-21 19:41:33 +0000

    Update 6872274481.lua

--- 6872274481.lua
+++ 6872274481.lua
@@ -10122,7 +10122,7 @@ runFunction(function()
 				task.spawn(function()
 					selectedPlayer = findPlayer()
 					killEntity()
-					task.wait(0.5)
+					task.wait(1.2)
 					repeat task.wait() until entityLibrary.character.Humanoid:GetState() ~= Enum.HumanoidStateType.Dead
 					warningNotification("Found player", "Teleporting to "..selectedPlayer.Name, 5)
 				end)
